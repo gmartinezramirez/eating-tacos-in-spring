941320374e5fb9afd836214052acb6dcd6a18caa
gmartinezramirez <gonzalo.martinez.ra@gmail.com>
2020-11-08 15:38:10 -0300

    feat(skeleton): add to model, ingredient, order and taco, all with the field validations

--- src/main/java/com/gmartinezramirez/eatingTacos/model/Ingredient.java
+++ src/main/java/com/gmartinezramirez/eatingTacos/model/Ingredient.java
@@ -1,4 +1,21 @@
 package com.gmartinezramirez.eatingTacos.model;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Ingredient {
+
+  private final Long id;
+  private final String name;
+  private final Type type;
+
+  public static enum Type {
+    WRAP,
+    PROTEIN,
+    VEGGIES,
+    CHEESE,
+    SAUCE
+  }
 }
